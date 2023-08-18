9f2742603d83bf90eae729d532e5db0fff476ecd
99TheDark <99TheDark@gmail.com>
2023-08-18 14:15:13 -0700

    Yeah, I definitely need to make a typer first, and numeric literals.

--- compiler/llvm.go
+++ compiler/llvm.go
@@ -1,7 +1,6 @@
 package compiler
 
 import (
-	"fmt"
 	"golang/parser"
 	"golang/typing"
 	"golang/utils"
@@ -12,42 +11,36 @@ import (
 )
 
 func search(m *ir.Module, expr parser.Expression) {
-	children := expr.Children()
-	if len(children) == 0 {
-		switch e := expr.(type) {
-		case parser.FunctionLiteral:
-			params := []*ir.Param{}
-			fmt.Println(e.Params.Values)
-			for _, param := range e.Params.Values {
-				if new, ok := param.(parser.Datatype); ok {
-					llvmparam := ir.NewParam(
-						new.Variable.Symbol,
-						typing.UnderlyingType(new.Type.Symbol).LLVMType(),
-					)
-					params = append(params, llvmparam)
-				} else {
-					panic("Not a datatype, typer not yet made")
-				}
+	switch e := expr.(type) {
+	case parser.FunctionLiteral:
+		params := []*ir.Param{}
+		for _, param := range e.Params.Values {
+			if new, ok := param.(parser.Datatype); ok {
+				llvmparam := ir.NewParam(
+					new.Variable.Symbol,
+					typing.UnderlyingType(new.Type.Symbol).LLVMType(),
+				)
+				params = append(params, llvmparam)
+			} else {
+				panic("Not a datatype, typer not yet made")
 			}
+		}
 
-			fun := m.NewFunc(
-				e.Name.Symbol,
-				types.I32,
-				params...,
-			)
-			bl := fun.NewBlock("")
-			bl.NewRet(constant.NewInt(types.I32, 0))
-		}
-	} else {
-		for _, child := range expr.Children() {
-			search(m, child)
-		}
+		fun := m.NewFunc(
+			e.Name.Symbol,
+			types.I32,
+			params...,
+		)
+		bl := fun.NewBlock("")
+		bl.NewRet(constant.NewInt(types.I32, 0))
 	}
 }
 
 func Assemble(ast parser.Program) string {
 	mod := ir.NewModule()
-	search(mod, ast)
+	for _, expr := range ast.Body {
+		search(mod, expr)
+	}
 
 	return mod.String()
 }
---
+++ io/script.ll
@@ -0,0 +1,4 @@
+define i32 @add(i32 %a, i32 %b) {
+0:
+	ret i32 0
+}
