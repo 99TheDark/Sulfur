2ddf830afd46d954b468995abca703ee95126b50
99TheDark <99TheDark@gmail.com>
2023-08-18 23:10:25 -0700

    Compiles to assembly (kind of)

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd io
+llvm-as script.ll -o script.bc
+llc script.bc -o script.s
+as -arch arm64 -o script.o script.s
+ld -o script script.o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -arch arm64
+./script
---
+++ io/script.s
@@ -0,0 +1,11 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 13, 0
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:                                ; %entry
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
